406c87de828d200b0faa007f07e804dec9282b08
Tobi <stevehappyboy@rocketmail.com>
2020-04-24 20:15:34 +0100

    gen code coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,6 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
 };
